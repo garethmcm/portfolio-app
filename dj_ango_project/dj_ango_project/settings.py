90caab7548dcbfa473dc27e87aada5cdb6e4211e
Gareth <gareth@Gareths-MacBook-Pro.local>
2024-05-15 09:57:21 +0100

    added staticfiles_build directory

--- dj_ango_project/dj_ango_project/settings.py
+++ dj_ango_project/dj_ango_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-yc@b7uwh2cca8wm^+cjz-!2q_m(-en97i#3_*ol33xl-6gng#c"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh']
 
@@ -115,8 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
